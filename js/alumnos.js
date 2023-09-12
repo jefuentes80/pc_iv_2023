f0fa2fe866049b0e65b64c6843be682f81a40e55
anleu04 <josueanleu@gmail.com>
2023-09-12 12:31:15 -0600

    Anleu04 v1: mi perfil

--- js/alumnos.js
+++ js/alumnos.js
@@ -36,10 +36,10 @@ export let alumnos = [
         github: ""
     },
     {
-        img: "",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0rAeS_UF92GQl4geYTCUy8i-CE60e4ZKljA&usqp=CAU",
         user_name: "Josue",
-        descripcion: "",
-        github: ""
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio voluptate perspiciatis magnam quas itaque maiores, voluptatibus fugit assumenda mollitia quae maxime. Accusantium aut laboriosam ea nostrum eligendi tempore illo.",
+        github: "https://github.com/anleu04"
     },
     {
         img: "",
