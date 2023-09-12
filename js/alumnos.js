8ee5e43a190c4c88c318c55b703aec96a455ebbe
D2007 <dacarranza@scl.edu.gt>
2023-09-12 12:25:04 -0600

    dacarranza v1: Mi Perfil

--- js/alumnos.js
+++ js/alumnos.js
@@ -6,10 +6,10 @@ export let alumnos = [
         github: "https://github.com/jefuentes80"
     },
     {
-        img: "",
-        user_name: "dereck",
-        descripcion: "",
-        github: ""
+        img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/12df0740-88bb-4c8e-8494-5b30eeefadb9/d65m9e7-19f765af-b7ad-4f22-8a20-0688b915d6c9.png/v1/fill/w_338,h_301/pou_png_by_maiiracat_d65m9e7-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MzAxIiwicGF0aCI6IlwvZlwvMTJkZjA3NDAtODhiYi00YzhlLTg0OTQtNWIzMGVlZWZhZGI5XC9kNjVtOWU3LTE5Zjc2NWFmLWI3YWQtNGYyMi04YTIwLTA2ODhiOTE1ZDZjOS5wbmciLCJ3aWR0aCI6Ijw9MzM4In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.2jGSfSbY9PSSl5XVNw696qqpTv92XvCUlmuIzIExmZ0",
+        user_name: "Dereck",
+        descripcion: "Hola, soy miembro del grupo de la grasa, y le se al minecraft tecnico",
+        github: "https://github.com/DereckAbraham"
     },
     {
         img: "",
