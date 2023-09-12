d0c4f595773b37f135749ee05094a7fd2448c1c4
jefuentes <jefuentes@slc.edu.gt>
2023-09-12 12:24:14 -0600

    jefuentes v1 : Mi perfil

--- js/alumnos.js
+++ js/alumnos.js
@@ -2,7 +2,7 @@ export let alumnos = [
     {
         img: "https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg",
         user_name : "jefuentes",
-        descripcion: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, veritatis nisi. Ducimus quaerat perspiciatis neque illo quam? Aperiam nemo animi esse placeat eum possimus, cum, quas mollitia harum alias sunt?",
+        descripcion: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit, veritatis nisi. Ducimus quaerat perspiciatis neque illo quam? Aperiam nemo animi esse placeat eum po",
         github: "https://github.com/jefuentes80"
     },
     {
