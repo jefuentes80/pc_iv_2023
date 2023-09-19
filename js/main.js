6be45c31c634b8765dad2416bb46ab89721119f3
jefuentes <jefuentes@slc.edu.gt>
2023-09-19 12:58:16 -0600

    integración de items al index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Hola mundo")
+import { alumnos } from "./alumnos.js";
+let main = document.querySelector(".main");
+
+alumnos.forEach((elemento) => {
+    console.log(elemento.user_name);
+
+    let componente = document.createElement("div");
+    componente.innerHTML = `
+        <a href="./${elemento.user_name}.html" class="user">
+        <img src="${elemento.img}" alt="">
+        <p>${elemento.user_name}</p>
+        </a>
+    `;
+    main.appendChild(componente);
+    
+})
